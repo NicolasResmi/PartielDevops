205876c9ce3ed6b3df3cb4f2dd26b5081ca331a4
NicolasResmi <nicolas.resmi@etu-webschoolfactory.fr>
2018-12-04 11:49:08 +0100

    Modification de main.go

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-
+	"os"
 	"wsf/devops/handler"
 )
 
 func main() {
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	myHandler := handler.NewHandler()
 
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: myHandler,
 	}
+
+	fmt.Println("test")
+	fmt.Println("Server is runnin at", port)
 
 	log.Fatal(server.ListenAndServe())
 }
